969acd5bc1f46e5fc4b7ae314296f88d7448d091
l-martinetti <luca.martinetti@outlook.it>
2025-06-28 17:52:22 +0200

    feat: add Videogame and Console controllers with authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ConsoleController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VideogameController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('videogame', VideogameController::class);
+    Route::resource('console', ConsoleController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
